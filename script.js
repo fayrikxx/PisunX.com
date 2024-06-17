2b426b130a7bf2672fa86e4f5384449da4f89dc0
fayrikxx <149820515+fayrikxx@users.noreply.github.com>
2024-06-17 18:22:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function executeLua() {
+    const script = document.getElementById('editor').value;
+    const outputElement = document.getElementById('output');
+    outputElement.innerHTML = '';
+
+    try {
+        fengari.load(script)();
+    } catch (e) {
+        outputElement.innerHTML = 'Error: ' + e.message;
+    }
+}
+
+function clearEditor() {
+    document.getElementById('editor').value = '';
+    document.getElementById('output').innerHTML = '';
+}
+
+// Redirect Lua print function to display in the output div
+fengari.interop.print = function(...args) {
+    const outputElement = document.getElementById('output');
+    outputElement.innerHTML += args.join(' ') + '\n';
+};
 
